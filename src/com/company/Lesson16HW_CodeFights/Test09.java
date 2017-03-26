c1b341d4d2c9881eeb875ac23c8168b2b9fe9641
Mila511 <jixareva@gmail.com>
2017-03-25 23:10:22 -0400

    Add_new_task_03

--- src/com/company/Lesson16HW_CodeFights/Test09.java
+++ src/com/company/Lesson16HW_CodeFights/Test09.java
@@ -9,81 +9,27 @@ package com.company.Lesson16HW_CodeFights;
 //        For sentence = "Man bites dog", the output should be
 //        reverseSentence(sentence) = "dog bites Man".
 //public class Test{
-//=============================================================================
-//    public static void main(String args[]){
-//        String Str1 = new String("Добро пожаловать на ProgLang.su");
-//        char[] Str2 = new char[7];
-//
-//        try{
-//            Str1.getChars(2, 9, Str2, 0);
-//            System.out.print("Скопированное значение: " );
-//            System.out.println(Str2);
-//
-//        }catch( Exception ex){
-//            System.out.println("Возникает исключение...");
-//        }
-//    }
-//}
 
 
 public class Test09 {
     public static void main(String[] args) {
-        String s = "Добро пожаловать на ProgLang.su";
-        String[] arrays = s.split(" ");
-        String  res = "";
+        //String sentence = "Добро пожаловать на ProgLang.su";
+        String sentence = "Man bites dog";
+
+        String[] arrays = sentence.split(" ");
+        String res = "";
 
         for (int i = arrays.length - 1; i >= 0; i--) {
+            //if (i == 0) {
+            //res += arrays[i];
+            // } else {
             res += arrays[i] + " ";
         }
-
-        System.out.println(res.trim());
-
-        //String sentence = new String("Man bites dog");
-        //String sentence = new String("abc ");
-        //String sentence = new String("a b c");
-//        String sentence = new String("abc");
-//
-//        String firstsub = new String();
-//        String lastsub = new String();
-//        int firstspc = -1;
-//        int lastspc = -1;
-//
-//        firstspc = sentence.indexOf(" ");
-//        lastspc = sentence.lastIndexOf(" ");
-//        System.out.println(firstspc+" "+lastspc);
-//
-//        if (firstspc == -1 && lastspc == -1) {//нет пробелов
-//            System.out.println("нет пробелов");
-//            sentence = sentence;
-//        }
-//        if (firstspc!=lastspc&&firstspc != -1 && lastspc != -1) {//2 есть 1-й и последний
-//            firstsub = sentence.substring(0, firstspc);
-//            lastsub = sentence.substring(lastspc + 1, sentence.length());
-//            sentence = sentence.replaceFirst(lastsub, firstsub).replaceFirst(firstsub, lastsub);
-//            System.out.println("2 есть 1-й и последний");
-//        }
-//
-//        if (firstspc==lastspc&&firstspc !=0) {//есть 1 , не на 0
-//            firstsub = sentence.substring(0, firstspc);
-//            lastsub = sentence.substring(firstspc, sentence.length());
-//            sentence = lastsub + firstsub;
-//            System.out.println("есть 1 , не на 0");
-//        }
-//        if (firstspc == lastspc&&firstspc == 0) {//есть 1 на 0
-//            firstsub = sentence.substring(0, firstspc);
-//            lastsub = sentence.substring(firstspc + 1, sentence.length());
-//            sentence = lastsub + firstsub;
-//            System.out.println("есть 1 на 0");
-//        }
-//        if (firstspc == lastspc && firstspc == sentence.length()) {//есть 1, на конце
-//            firstsub = sentence.substring(0, firstspc);
-//            lastsub = sentence.substring(firstspc + 1, sentence.length());
-//            sentence = lastsub + firstsub;
-//            System.out.println("есть 1, на конце");
-//        }
-//        System.out.println(sentence);
+        res = res.trim();
+        System.out.println(res);
     }
 }
+
 
 
 
---
+++ src/com/company/Lesson16HW_CodeFights/Test10.java
@@ -0,0 +1,61 @@
+package com.company.Lesson16HW_CodeFights;
+
+/**
+ * Created by Mila on 3/23/2017.
+ */
+
+//    For s = "2-4a0r7-4k" and k = 4, the output should be
+//            stringReformatting(s, k) = "24a0-r74k";
+//
+//            The input string "2-4a0r7-4k" is split into three groups with lengths of 1, 5 and 2. Since k = 4, you need to split the string into two groups of 4 characters each, making the output string "24A0-R74k".
+//
+//            For s = "2-4a0r7-4k" and k = 3, the output should be
+//            stringReformatting(s, k) = "24-a0r-74k".
+//
+//            Given the same input string and k = 3, split the string into groups of 2, 3, and 3 characters to get the output string of "24-a0r-74k".
+
+
+public class Test10 {
+    public static void main(String[] args) {
+        //int k = 8;//-
+        int k = 7;//+
+        //int k = 6;//+
+        //int k = 5;//+
+        //int k = 4;//-
+        // int k = 3;//+
+        //int k = 2;//-
+        //int k = 1;//-
+
+        String s = "2-4a0r7-4k";
+        String res = "";
+        int times=0;
+        s = s.replaceAll("-", "");
+
+   if((s.length()%k)==0){
+       times = s.length()/k;
+
+
+   }else {
+       double time = Math.round(s.length()/k+0.5);
+       times = (int)time;
+
+   }
+
+      String[] strarr = new String[times];
+        for (int i = 0; i <strarr.length; i++) {
+            if (i ==0) {
+                strarr[i] = s.substring(0, s.length() - (times - 1) * k);
+
+
+            } else {
+                strarr[i] = "-"+s.substring(strarr[0].length()+(i-1)*k, strarr[0].length()+i*k);
+
+            }
+            res=res+strarr[i];
+        }
+        System.out.println(res);
+
+
+        }
+    }
+
